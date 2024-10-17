5871d4784e4d0fd1a89d6dd14623d34226cca929
dgusfr <diegogustavo.fr@gmail.com>
2024-10-17 07:55:56 -0300

    feat(tabuada)

--- loops/7.js
+++ loops/7.js
@@ -1,10 +1,8 @@
 let numero = parseInt(prompt("Digite um número:"));
 
-// Verifica se o valor inserido é um número válido
 if (!isNaN(numero)) {
   document.write("<h2>Tabuada do " + numero + ":</h2>");
 
-  // Loop for para calcular e imprimir a tabuada do número
   for (let i = 1; i <= 10; i++) {
     document.write(numero + " x " + i + " = " + numero * i + "<br>");
   }
