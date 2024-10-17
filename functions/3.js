7610b57f05fdf6062b29469d21c71b57b92b4c54
dgusfr <diegogustavo.fr@gmail.com>
2024-10-17 08:07:41 -0300

    feat(fat)

--- functions/3.js
+++ functions/3.js
@@ -13,17 +13,3 @@ function calcularFatorial(numero) {
 const numero = 5;
 const resultado = calcularFatorial(numero);
 console.log(`O fatorial de ${numero} é: ${resultado}`);
-
-// Com numero = 5 e fatorial = 1.
-
-// 1ª iteração (i = 2) e (fatorial=1)
-// fatorial *= i -> fatorial = 1 * 2 = 2.
-
-// 2ª iteração (i = 3) e (fatorial=2)
-// fatorial *= i -> fatorial = 2 * 3 = 6.
-
-// 3ª iteração (i = 4) e (fatorial=6)
-// fatorial *= i -> fatorial = 6 * 4 = 24.
-
-// 4ª iteração (i = 5) e (fatorial=24)
-// fatorial *= i -> fatorial = 24 * 5 = 120.
