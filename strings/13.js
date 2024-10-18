c6ce33927a45e3affe933788332e3dcf65d51721
dgusfr <diegogustavo.fr@gmail.com>
2024-10-18 09:07:29 -0300

    changes

--- strings/13.js
+++ strings/13.js
@@ -14,18 +14,14 @@ function contarOcorrencias(string, letra) {
 const string = "Olá, mundo! Esta é uma string de exemplo.";
 const letra = "o";
 console.log(contarOcorrencias(string, letra));
-//Saída: 3
 
 //**************************UTILIZANDO EXPRESSÕES REGULARES**************************
 
 function contaOcorrenciasRegular(string, letra) {
-  // Fazer a correspondência global (modificador g) e insensível a maiúsculas e minúsculas (modificador i).
   const ocorrencias = string.match(new RegExp(letra, "gi"));
-  // Se não houver ocorrências, retornamos 0, caso contrário, retornamos o número de ocorrências encontradas
   return ocorrencias ? ocorrencias.length : 0;
 }
 
 const stringExemplo = "Olá, mundo! Esta é uma string de exemplo.";
 const letraExemplo = "o";
 console.log(contaOcorrenciasRegular(stringExemplo, letraExemplo));
-//Saída: 3
